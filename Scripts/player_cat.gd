c97feb04cf314b657baf1e046c99f40d90f862f7
Eden <ecoding@pm.me>
2024-02-25 20:08:48 -0800

    Small adjustments

--- Scripts/player_cat.gd
+++ Scripts/player_cat.gd
@@ -4,17 +4,17 @@ extends CharacterBody2D
 @export  var starting_direction : Vector2 = Vector2(0, 1)
 #parameters/Idle/blend_position
 
-@onready var animation_tree = $AnimationTree
+#@onready var animation_tree = $AnimationTree
 
-func _ready():
-	animation_tree.set("parameters/Idle/blend_position", starting_direction) 
+#func _ready():
+	#animation_tree.set("parameters/Idle/blend_position", starting_direction) 
 func _physics_process(_delta):
 	# Get input direction
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
-	print(input_direction)
+	#print(input_direction)
 	
 	# Update velocity
 	velocity = input_direction * move_speed
